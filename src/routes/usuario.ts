91df6bb83907e53907e79da41be9e5ba6973e9be
Your Name <you@example.com>
2026-09-24 09:29:03 -0300

    ...

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -1,5 +1,6 @@
 import { db } from "../database-connect"
 import { Hono } from 'hono'
+import * as controller from "../controllers/usuario"
 
 const usuarios = new Hono()
 
@@ -9,34 +10,7 @@ usuarios.get('/usuario', (c) => {
   return c.json(data)
 })
 
-usuarios.post('/usuario', async (c) => {
-  try {
-    const body = await c.req.json()
-    if (!body.nome)
-      return c.json("faltou nome", { status: 400 })
-    if (!body.idade || isNaN(body.idade))
-      return c.json("idade precisa ser um número inteiro", { status: 400 })
-    if (!body.email || !body.email.includes("@"))
-      return c.json("precisa incluir email válido", { status: 400 })
-    const query = db.query(`
-          INSERT INTO usuarios (nome, idade, email) 
-          VALUES ($nome, $idade, $email) 
-        `)
-    const resp = query.run({
-      $nome: body.nome,
-      $idade: body.idade,
-      $email: body.email,
-    })
-    return c.json(resp)
-  } catch (e: any) {
-    if (e instanceof SyntaxError)
-      return c.json("json mal formatado", { status: 400 })
-    return c.json({
-      message: "Internal Server Error",
-      detail: e.message
-    }, { status: 500 })
-  }
-})
+usuarios.post('/usuario', controller.create)
 
 usuarios.get('/usuario/:id', async (c) => {
   const query = db.query(`SELECT * FROM usuarios WHERE id=:id`)
